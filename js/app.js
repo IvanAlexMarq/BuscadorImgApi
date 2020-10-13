7cea16f37e092d522b5ea6842daffcefe2e07c83
IvanAlexMarq <70303520+IvanAlexMarq@users.noreply.github.com>
2020-10-12 21:24:30 -0400

    show results in html

--- js/app.js
+++ js/app.js
@@ -52,4 +52,25 @@ function buscarImagenes(termino) {
 
 function mostrarImagenes(imagenes) {
     console.log(imagenes);
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    //Iterar sobre el arreglo de imagenes y construir el html
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img src="${previewURL}" class="w-full">
+                <div class="p-4">
+                    <p class="font-bold">${likes}<span class="font-light">Me Gusta</span></p>
+                    <p class="font-bold">${views}<span class="font-light">Veces Vista</span></p>
+                    <a href="${largeImageURL}" class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1" target="_blanck">Ver Imagen</a>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+
 }
