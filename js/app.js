ea6807a9e639b57189ca14c6667765410a5b6c17
IvanAlexMarq <70303520+IvanAlexMarq@users.noreply.github.com>
2020-10-13 10:11:26 -0400

    Mostrando la cantidad de páginas del Paginador

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
 
 const registrosPorPagina = 40;
 let totalPaginas;
@@ -45,7 +46,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '18682550-0c4675b78cad8c2aaee35d10c';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -59,7 +60,7 @@ function buscarImagenes(termino) {
 
 //Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
 function* crearPaginador(total) {
-    console.log(total);
+
     for (let i = 1; i <= total; i++) {
         yield i;
     }
@@ -71,7 +72,7 @@ function calcularPaginas(total) {
 }
 
 function mostrarImagenes(imagenes) {
-    console.log(imagenes);
+    //console.log(imagenes);
     while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
@@ -92,9 +93,29 @@ function mostrarImagenes(imagenes) {
         </div>
         `;
     })
+
+    //limpiar el paginador previo
+    while (paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild)
+    }
+
+    //Generamos el nuevo html
     imprimirPaginador();
 }
 
 function imprimirPaginador() {
     iterador = crearPaginador(totalPaginas);
+
+    while (true) {
+        const { value, done } = iterador.next();
+        if (done) return;
+
+        //caso controrio genera un boton por cada elemento en el generador
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+        paginacionDiv.appendChild(boton);
+    }
 }
